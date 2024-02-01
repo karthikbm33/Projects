525875eab650ffeee71ddbf3ac888824ebd9628c
Karthikshet <142784746+Karthikshet@users.noreply.github.com>
2024-02-01 11:45:53 -0100

    Add files via upload

---
+++ C#/Hello.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Lab
+{
+    class Hello 
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
