d185b778029432a615ae519439033cd897b12740
Karthikshet <142784746+Karthikshet@users.noreply.github.com>
2024-02-02 07:55:03 -0100

    Add files via upload

---
+++ Java/NetBeansProjects/first/src/main/java/com/mycompany/kar/Kar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kar;
+
+/**
+ *
+ * @author Ananth
+ */
+public class Kar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
